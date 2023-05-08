7a171597bdf0be164f5bca743da6ffaa095ebe54
HumanIearning <kimdh3896@naver.com>
2023-05-08 17:53:50 +0900

    :sparkles: :feat: pad data

--- ai/data_loader.py
+++ ai/data_loader.py
@@ -7,16 +7,8 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 
+import sentencepiece as spm
 
-# class DotDict(dict):
-#     """dot.notation access to dictionary attributes"""
-#
-#     def __getattr__(*args):
-#         val = dict.get(*args)
-#         return DotDict(val) if type(val) is dict else val
-#
-#     __setattr__ = dict.__setitem__
-#     __delattr__ = dict.__delitem__
 
 def get_value_for_stacked_dictionary(data, keys):
     if len(keys) == 1:
@@ -25,13 +17,12 @@ def get_value_for_stacked_dictionary(data, keys):
         try:
             return get_value_for_stacked_dictionary(data[keys[0]], keys[1:])
         except Exception as e:
-            print(e)
-            return None
+            return 'None'
 
 
 class PacketDataset(Dataset):
 
-    def __init__(self, filepath):
+    def __init__(self, filepath, max_length=90):
         print("loading data...")
         self.data = pd.read_json(filepath)['_source']
 
@@ -45,18 +36,34 @@ class PacketDataset(Dataset):
                             "_source:layers:tcp:tcp.srcport",
                             "_source:layers:tcp:tcp.dstport",
                             "_source:layers:udp:udp.srcport",
-                            "_source:layers:udp:udp.dstport"]
+                            "_source:layers:udp:udp.dstport",
+                            "_source:layers:frame:frame.protocols",
+                            "_source:layers:http:http.host"]
+        self.sp = spm.SentencePieceProcessor()
+        self.vocab_file = "packet.model"
+        self.sp.load(self.vocab_file)
 
+        # encoding
         for i, packet in enumerate(self.data.values):
             temp = []
             for packet_name in self.used_packet:
                 packet_name = packet_name.split(":")
                 temp.append(get_value_for_stacked_dictionary(packet, packet_name[1:]))
-            self.data[i] = temp
+
+            temp = self.sp.encode_as_ids('<cls>'+'<sep>'.join(temp))
+            self.data[i] = [temp, len(temp)]
+
+        # padding
+        for data in self.data:
+            gap = max_length - data[1]
+            if gap > 0:
+                data[0] = data[0] + [3] * gap   # [3] is pad token id
+            else:
+                continue
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        pass
+        return self.data[idx]
 
